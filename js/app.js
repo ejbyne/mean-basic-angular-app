e3cffe891385533a306f51d3deef321effc203ba
ed <ejbyne@gmail.com>
2015-01-16 10:59:22 +0000

    added addComputer function and form for adding computers

--- js/app.js
+++ js/app.js
@@ -2,4 +2,20 @@ angular.module('firstApp', [])
 
 .controller('mainController', function() {
   var vm = this;
-})
+  vm.message = 'Hey there! Come and see how good I look!';
+  vm.computers = [
+    { name: 'Macbook Pro', colour: 'Silver', nerdness: 7 },
+    { name: 'Yoga 2 Pro', colour: 'Gray', nerdness: 6 },
+    { name: 'Chromebook', colour: 'Black', nerdness: 5 }
+  ];
+  vm.computerData = {};
+
+  vm.addComputer = function() {
+    vm.computers.push({
+      name: vm.computerData.name,
+      colour: vm.computerData.colour,
+      nerdness: vm.computerData.nerdness
+    });
+    vm.computerData = {};
+  }
+});
